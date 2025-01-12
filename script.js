e1449822179123d29d5774c413d8bb77d19e521f
Luis Josue Suastegui Nranjo <luis.suasteguin@ug.edu.ec>
2025-01-11 22:34:35 -0500

    imagen

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 // script.js
 document.addEventListener('DOMContentLoaded', () => {
-    const button = document.getElementById('modo-contraste');
-    button.addEventListener('click', () => {
-        document.body.classList.toggle('alto-contraste');
+    const imagenes = document.querySelectorAll('.imagen-galeria');
+    const lightbox = document.getElementById('lightbox');
+    const imagenLightbox = document.getElementById('imagen-lightbox');
+    const cerrarLightbox = document.getElementById('cerrar-lightbox');
+
+    // Abre el lightbox al hacer clic en una imagen
+    imagenes.forEach(imagen => {
+        imagen.addEventListener('click', () => {
+            imagenLightbox.src = imagen.src; // Asigna la imagen al lightbox
+            lightbox.style.display = 'flex'; // Muestra el lightbox
+        });
+    });
+
+    // Cierra el lightbox al hacer clic en el botón de cierre
+    cerrarLightbox.addEventListener('click', () => {
+        lightbox.style.display = 'none';
+    });
+
+    // Cierra el lightbox al hacer clic fuera de la imagen
+    lightbox.addEventListener('click', (e) => {
+        if (e.target !== imagenLightbox) {
+            lightbox.style.display = 'none';
+        }
     });
 });
